4f6a0d7c3d9cdef7875536495bf5a43be4140838
Iegor-Funtusov <funtushan@gmail.com>
2022-02-08 20:03:58 +0200

    fix ant

--- unit_2_compilation/java/ant/setantenv.sh
+++ unit_2_compilation/java/ant/setantenv.sh
@@ -23,7 +23,7 @@ fi
 export PLATFORM_HOME
 PLATFORM_HOME=$(pwd)
 export ANT_OPTS="-Xmx2g -Dfile.encoding=UTF-8"
-export ANT_HOME="$PLATFORM_HOME/apache-ant-1.10.12"
+export ANT_HOME="$PLATFORM_HOME/apache-ant"
 chmod +x "$ANT_HOME/bin/ant"
 chmod +x "$PLATFORM_HOME/license.sh"
 case "$PATH" in
