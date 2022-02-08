b1fe3e58cec3ad694bf5281872cdf88bfca5c342
Iegor Funtusov <funtushan@gmail.com>
2022-02-08 13:26:50 +0200

    learn compile

--- unit_2_compilation/java/simple/problem/src/a/Main.java
+++ unit_2_compilation/java/simple/problem/src/a/Main.java
@@ -5,6 +5,7 @@ import a.b.Test;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Main.main");
         Test test = new Test();
         test.hello();
     }
